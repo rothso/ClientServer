8a5bb10ce9360ee4dad39da93ff7d631782758c5
rajp4 <rajp4445@gmail.com>
2019-02-04 19:07:49 -0500

    Add read on client and implement command on server. Need to figure out how to read multiple commands on server from the client

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,8 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -10,14 +13,20 @@ public class Client {
         // Listening socket
         Socket socket = new Socket("127.0.0.1", 8000);
         PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-        writer.println("1");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         while(true)
         {
             int userInput=displayMenu();    // displays menu and takes care of the invalid input
             writer.print(userInput);        //if the inp=7, we will tell the server to stop!
             if(userInput==7)
                 break;
+            Scanner scan= new Scanner(reader);
+            while(scan.hasNext())
+            {
+                System.out.println(scan.next());
+            }
         }
+
     }
     public static int displayMenu() {
         Scanner usrInput = new Scanner(System.in);
@@ -32,7 +41,6 @@ public class Client {
         int inp = usrInput.nextInt();
         if (inp > 0 && inp < 8) {
             return inp;
-
         } else {
             System.out.println("Invalid option! Try again");
             return displayMenu();
--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
@@ -12,18 +13,42 @@ public class Server {
         ServerSocket serverSocket = new ServerSocket(8000);
         Socket clientSocket = serverSocket.accept();
         BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        PrintWriter printOnClient = new PrintWriter(clientSocket.getOutputStream());
         Scanner scanner = new Scanner(reader);
 
         String command = scanner.next();
-
-        if (command.equals("1")) {
-            Process process = Runtime.getRuntime().exec("git --version");
+        String realCommand="";
+        int c = Integer.parseInt(command);
+        switch (c){
+            case 1:
+                realCommand="date";
+                break;
+            case 2:
+                realCommand="uptime";
+                break;
+            case 3:
+                realCommand="free";
+                break;
+            case 4:
+                realCommand="netstat";
+                break;
+            case 5:
+                realCommand="who";
+                break;
+            case 6:
+                realCommand="top";
+                        break;
+            default:
+                realCommand="quit";
+        }
+        if (!realCommand.equals("quit")) {
+            Process process = Runtime.getRuntime().exec(realCommand);
             BufferedReader commandReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             Scanner commandScanner = new Scanner(commandReader);
-
             while (commandScanner.hasNext()) {
-                System.out.println(commandScanner.next());
+                printOnClient.write(commandScanner.next());
             }
         }
+
     }
 }
