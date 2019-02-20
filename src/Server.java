72c9727555467c44665aae914b8e930733599991
rajp4 <rajp4445@gmail.com>
2019-02-19 22:05:35 -0500

    Print the diagnostic messages on the server's console

--- src/Server.java
+++ src/Server.java
@@ -14,18 +14,21 @@ public class Server {
 
         // This loop lets the server accept a new client when the current client disconnects
         while (true) {
+            System.out.println("Waiting to connect...");
             Socket clientSocket = serverSocket.accept();
             System.out.println("Client connected.");
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             PrintWriter printOnClient = new PrintWriter(clientSocket.getOutputStream(), true);
+            System.out.println("running/Waiting for the command");
             Scanner scanner = new Scanner(reader);
             String realCommand;
 
             while (scanner.hasNextLine()) {
-                System.out.println("running/ Waiting");
+//                System.out.println("running/Waiting for the command");
                 String command = scanner.nextLine();
-                System.out.println(command);
+                System.out.println("Command received");
+//                System.out.println(command);
 
                 int c = Integer.parseInt(command);
                 switch (c) {
@@ -51,10 +54,11 @@ public class Server {
                         realCommand = "quit";
                 }
                 if (!realCommand.equals("quit")) {
+                    System.out.println("Running the command on the machine");
                     Process process = Runtime.getRuntime().exec(realCommand);
                     BufferedReader commandReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
                     Scanner commandScanner = new Scanner(commandReader);
-
+                    System.out.println("Sending the information to the client");
                     while (commandScanner.hasNextLine()) {
                         String line = commandScanner.nextLine();
                         printOnClient.println(line);
@@ -62,7 +66,9 @@ public class Server {
                     }
 
                     // Tell the client to stop listening
+                    System.out.println("Ack sent");
                     printOnClient.println("ACK");
+                    System.out.println("running/Waiting for the command");
                 } else {
                     break;
                 }
